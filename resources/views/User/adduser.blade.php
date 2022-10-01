028bc79fe24783f8153c402975fed2ed60204c9c
Yared11000010 <yaredayele67@gmail.com>
2022-10-01 16:08:08 -0400

    i edited user managements

--- resources/views/User/adduser.blade.php
+++ resources/views/User/adduser.blade.php
@@ -10,7 +10,7 @@
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">User Managements</a></li>
+              <li class="breadcrumb-item"><a href="#">Users Managements</a></li>
               <li class="breadcrumb-item active">Add User</li>
             </ol>
           </div>
